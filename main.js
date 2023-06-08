4daf15ac0c4ddccfd8573ba5bc7ea53a0f735d97
Mario Roca <marioroca@MacBook-Pro-di-Mario.local>
2023-06-08 23:56:29 +0200

    ip address changed

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*-----------
     VARIABILI 
 ------------*/
-const API_URL = "https://151.34.10.133:3000";
+const API_URL = "https://151.35.0.127:3000";
 const MODEL = "gpt-3.5-turbo";
 
 const loader = document.querySelector('.loading');
